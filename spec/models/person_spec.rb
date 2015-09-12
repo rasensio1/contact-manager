34cee62fdeb1257e792a1795c0451093e19f2a38
Ryan Asensio <ryan.asensio@gmail.com>
2015-09-12 15:52:07 -0600

    legit person is valid

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
+  it 'is valid' do
+    person = Person.new(first_name: "Ryan", last_name: "Asensio")
+    expect(person).to be_valid
+  end
+
   it 'is invalid withot a first name' do
     person = Person.new(first_name: nil)
     expect(person).not_to be_valid
